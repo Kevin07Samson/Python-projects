aea1d012f25218086092a2c56dbd5e784238e207
kevinsamson <kevin07samson@gmail.com>
2026-08-27 17:51:05 +0530

    Add Day 26 - NATO alphabet project

---
+++ 100 days of code/Day 26 - NATO alphabet start/main.py
@@ -0,0 +1,53 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+nato_dic = {"A": "Alfa",
+            "B": "Bravo",
+            "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo","F":"Foxtrot","G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliett",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-ray",
+    "Y": "Yankee",
+    "Z": "Zulu"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word:").upper()
+
+nato_list = [nato_dic[values] for values in user_input if values in nato_dic]
+print(nato_list)
